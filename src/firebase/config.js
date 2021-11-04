86b3ae4badcf49cb370f162312a55c29e535651b
franciscodimo <fdimo@udesa.edu.ar>
2021-11-04 10:53:35 -0300

    firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import firebase from 'firebase';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBlqDJcvQsycjniN9XKn4F8CW_kZ0cVr_o",
-  authDomain: "proyectofirebase-6e863.firebaseapp.com",
-  projectId: "proyectofirebase-6e863",
-  storageBucket: "proyectofirebase-6e863.appspot.com",
-  messagingSenderId: "730645675627",
-  appId: "1:730645675627:web:f33a54a8bd272ca6f5bb2e"
+  apiKey: "AIzaSyBBzbZcuKczVCD0sWyHLNfU9nFAPe5TwhQ",
+  authDomain: "proyecto-react-native-6c2c4.firebaseapp.com",
+  projectId: "proyecto-react-native-6c2c4",
+  storageBucket: "proyecto-react-native-6c2c4.appspot.com",
+  messagingSenderId: "205894965206",
+  appId: "1:205894965206:web:1f0809002519290757b677"
 };
 
 
